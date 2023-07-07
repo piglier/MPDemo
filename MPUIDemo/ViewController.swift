31fac7b4860efdd1034c3e3de8388fb5b3253680
JasonZhu <JasonZhu@taiwanmobile.com>
2023-07-07 16:27:52 +0800

    [add]print viewDidLoad2

--- MPUIDemo/ViewController.swift
+++ MPUIDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("viewDidLoad")
+        print("viewDidLoad2")
     }
     
 }
